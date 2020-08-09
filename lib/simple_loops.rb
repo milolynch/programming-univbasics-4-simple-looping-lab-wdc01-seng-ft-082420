600a32fb933378c83a49a2e2419b8ab2872050f6
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-09 22:33:01 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,35 @@
 # Write your methods here
+
+  
+def loop_message_five_times(s)
+  #takes in a message and outputs it five times (FAILED - 1)
+  c = 0
+  while c < 5
+    puts s
+    c += 1
+  end
+end
+
+def loop_message_n_times(s, n)
+  c = 0
+  while c < n
+    puts s
+    c += 1
+  end
+end
+
+def output_array (s)
+    c = 0
+  while c < 10
+    puts s
+    c += 1
+  end
+end
+
+def return_string_array(a)
+  new_a = []
+  for element in a
+    new_a.push(element.to_s)
+  end
+  return new_a
+end
